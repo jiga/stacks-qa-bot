b340af455ef33a05f67cceeea319bf9fcc0c2b64
j2p2 <i.digg.tech@gmail.com>
2023-04-04 00:40:09 -0700

    add stacks-blockchain, stacks and clarity-lang book repo

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
 
 store.index = index
-chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(temperature=0), chain_type="stuff", retriever=store.as_retriever())
+chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(temperature=0), chain_type="stuff", retriever=store.as_retriever(search_kwargs={"k": 10}))
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="Stacks Q&A Bot", page_icon=":robot_face:")
@@ -31,12 +31,19 @@ def convert_to_links(s):
     result = []
 
     for substring in substrings:
-        if '/' in substring:
-            prefix, content = substring.split('/', 1)
+        # print(substring)
+        if '/' in substring and 'N/A' not in substring:
+            prefix, content = substring.strip()[7:].split('/', 1)
             if prefix.strip().startswith("docs"):
                 link = "https://github.com/stacks-network/docs/blob/master/" + content
             elif prefix.strip().startswith("sips"):
                 link = "https://github.com/stacksgov/sips/blob/main/" + content
+            elif prefix.strip().startswith("stacks"):
+                link = "https://github.com/stacks-network/stacks/blob/master/" + content
+            elif prefix.strip().startswith("stacks-blockchain"):
+                link = "https://github.com/stacks-network/stacks-blockchain/blob/main/" + content
+            elif prefix.strip().startswith("book"):
+                link = "https://github.com/clarity-lang/book/blob/main/" + content
             else:
                 link = substring
         else:
@@ -54,7 +61,7 @@ def get_text():
 user_input = get_text()
 
 if user_input:
-    result = chain({"question": user_input})
+    result = chain({"input_documents": store, "question": user_input})
     sources = convert_to_links(result['sources'])
     output = f"#### Answer: ####\n {result['answer']}\nSources: {sources}"
 
